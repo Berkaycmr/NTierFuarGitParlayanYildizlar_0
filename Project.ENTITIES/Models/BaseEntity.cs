d757b9b1bc2dbace67d1df865af078d36588ff68
Berkay Çamur <berkaycmr@gmail.com>
2023-03-02 19:46:25 +0300

    BaseEntity class'ı ve DataStatus enum'ı oluşturuldu.

---
+++ Project.ENTITIES/Models/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using Project.ENTITIES.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public abstract class BaseEntity
+    {
+        public int ID { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public DataStatus Status { get; set; }
+
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+            Status = DataStatus.Inserted;
+        }
+    }
+}
