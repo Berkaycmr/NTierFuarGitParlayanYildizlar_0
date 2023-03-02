cfaf5cfea339394a9a7290c2c1633f34ad545bd0
Berkay Çamur <berkaycmr@gmail.com>
2023-03-02 19:49:51 +0300

    AppUser ve AppUserProfile class'ları eklendi.

---
+++ Project.ENTITIES/Models/AppUserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class AppUserProfile:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        //Relational Properties
+        public AppUser AppUser { get; set; }
+    }
+}
