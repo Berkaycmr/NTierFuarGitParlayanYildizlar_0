956a19350de5286f868c1700b8c26b943587d359
baduwan <batuhan.doymaz@gmail.com>
2023-03-03 15:34:48 +0300

    DAL katmanımızın işlemleri tamamlandı.

--- Project.DAL/ContextClasses/MyContext.cs
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project.ENTITIES.Models;
+using Project.MAP.Options;
 
 namespace Project.DAL.ContextClasses
 {
-    internal class MyContext
+    public class MyContext : DbContext
     {
+        public MyContext() : base("MyConnection")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new AppUserProfileMap());
+            modelBuilder.Configurations.Add(new CustomerMap());
+            modelBuilder.Configurations.Add(new FairMap());
+            modelBuilder.Configurations.Add(new LocationMap());
+            modelBuilder.Configurations.Add(new SaloonMap());
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppUserProfile> AppUserProfile { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Fair> Fairs { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Saloon> Saloons { get; set; }
     }
 }
