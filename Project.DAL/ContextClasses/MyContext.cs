bcfcd6b2a654940853adfc70faed80f113ccbc7e
baduwan <batuhan.doymaz@gmail.com>
2023-03-04 17:18:50 +0300

    Silinen Classlar, tüm katmanlardan kaldırıldı.

--- Project.DAL/ContextClasses/MyContext.cs
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -28,41 +28,22 @@ namespace Project.DAL.ContextClasses
 
             modelBuilder.Configurations.Add(new AppUserMap());
             modelBuilder.Configurations.Add(new AppUserProfileMap());
-            modelBuilder.Configurations.Add(new CafeMap());
-            modelBuilder.Configurations.Add(new CafeRequestMap());
             modelBuilder.Configurations.Add(new CustomerMap());
-            modelBuilder.Configurations.Add(new FairMap());
-            modelBuilder.Configurations.Add(new LocationMap());
-            modelBuilder.Configurations.Add(new MeetingRoomMap());
-            modelBuilder.Configurations.Add(new MeetingRoomRequestMap());
             modelBuilder.Configurations.Add(new RequestMap());
             modelBuilder.Configurations.Add(new SaloonMap());
-            modelBuilder.Configurations.Add(new SaloonCustomerMap());
             modelBuilder.Configurations.Add(new SaloonSponsorMap());
-            modelBuilder.Configurations.Add(new SectionMap());
             modelBuilder.Configurations.Add(new SponsorMap());
-            modelBuilder.Configurations.Add(new ToiletMap());
-            modelBuilder.Configurations.Add(new ToiletRequestMap());
+            
         }
 
         //Tablo olabilmeleri için DbSetlerimizi tanımlıyoruz.
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<AppUserProfile> AppUserProfiles { get; set; }
-        public DbSet<Cafe> Cafes { get; set; }
-        public DbSet<CafeRequest> CafeRequests { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Fair> Fairs { get; set; }
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<MeetingRoom> MeetingRooms { get; set; }
-        public DbSet<MeetingRoomRequest> MeetingRoomRequests { get; set; }
         public DbSet<Request> Requests { get; set; }
         public DbSet<Saloon> Saloons { get; set; }
-        public DbSet<SaloonCustomer> SaloonCustomers { get; set; }
         public DbSet<SaloonSponsor> SaloonSponsors { get; set; }
-        public DbSet<Section> Sections { get; set; }
         public DbSet<Sponsor> Sponsors { get; set; }
-        public DbSet<Toilet> Toilets { get; set; }
-        public DbSet<ToiletRequest> ToiletRequests { get; set; }
 
     }
 
